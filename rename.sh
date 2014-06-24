7018de98e7edaf5259992941bf968f93b5159c61
eigendreams <jakob.culebro@gmail.com>
2014-06-24 18:27:51 -0500

    renaming fixes

--- rename.sh
+++ rename.sh
@@ -1,16 +1,18 @@
 user_name=$USER
 host_name=$HOSTNAME
+ouser_name='jakob'
+ohost_name='jakob-think'
 
-sed -i -e 's/jakob/$user_name/g' /home/$user_name/workspace/catkin/src/finder/launch/machines_test.launch
-sed -i -e 's/jakob-think/$host_name/g' /home/$user_name/workspace/catkin/src/finder/launch/machines_test.launch
+sed -i -e 's/$ohost_name/$host_name/g' /home/$user_name/workspace/catkin/src/finder/launch/machines_test.launch
+sed -i -e 's/$ouser_name/$user_name/g' /home/$user_name/workspace/catkin/src/finder/launch/machines_test.launch
 
-sed -i -e 's/jakob/$user_name/g' /home/$user_name/workspace/rosbuild/.rosinstall
-sed -i -e 's/jakob-think/$host_name/g' /home/$user_name/workspace/rosbuild/.rosinstall
+sed -i -e 's/$ohost_name/$host_name/g' /home/$user_name/workspace/rosbuild/.rosinstall
+sed -i -e 's/$ouser_name/$user_name/g' /home/$user_name/workspace/rosbuild/.rosinstall
 
-sed -i -e 's/jakob/$user_name/g' /home/$user_name/workspace/rosbuild/setup.bash
-sed -i -e 's/jakob-think/$host_name/g' /home/$user_name/workspace/rosbuild/setup.bash
+sed -i -e 's/$ohost_name/$host_name/g' /home/$user_name/workspace/rosbuild/setup.bash
+sed -i -e 's/$ouser_name/$user_name/g' /home/$user_name/workspace/rosbuild/setup.bash
 
-sed -i -e 's/jakob/$user_name/g' /home/$user_name/workspace/rosbuild/setup.sh
-sed -i -e 's/jakob-think/$host_name/g' /home/$user_name/workspace/rosbuild/setup.sh
+sed -i -e 's/$ohost_name/$host_name/g' /home/$user_name/workspace/rosbuild/setup.sh
+sed -i -e 's/$ouser_name/$user_name/g' /home/$user_name/workspace/rosbuild/setup.sh
 
 
