6b826bd6721c3e325a87c297d2685fa79a207bb7
eigendreams <jakob.culebro@gmail.com>
2014-06-25 17:07:10 -0500

    fixes

--- rename.sh
+++ rename.sh
@@ -1,8 +1,11 @@
 user_name=$USER
 host_name=$HOSTNAME
 
-sed -i -e "s/jakob-think/$host_name/" /home/$user_name/workspace/catkin/src/finder/launch/machines_test.launch
-sed -i -e "s/jakob/$user_name/" /home/$user_name/workspace/catkin/src/finder/launch/machines_test.launch
+sed -i -e "s/jakob-think/$host_name/" /home/$user_name/workspace/catkin/src/finder/launch/*.launch
+sed -i -e "s/jakob/$user_name/" /home/$user_name/workspace/catkin/src/finder/launch/*.launch
 
 sed -i -e "s/jakob-think/$host_name/"  /home/$user_name/workspace/rosbuild/.rosinstall
 sed -i -e "s/jakob/$user_name/" /home/$user_name/workspace/rosbuild/.rosinstall
+
+sed -i -e "s/jakob-think/$host_name/"  /home/$user_name/workspace/rosbuild/setup.*
+sed -i -e "s/jakob/$user_name/" /home/$user_name/workspace/rosbuild/setup.*
