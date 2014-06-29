bb7e8e1a864e101d89d6c79524f664eef333cd9a
eigendreams <jakob.culebro@gmail.com>
2014-06-29 00:34:48 -0500

    ir fixes

--- catkin/src/finder/scripts/ir_node.py
+++ catkin/src/finder/scripts/ir_node.py
@@ -64,7 +64,7 @@ class IR_Node():
             """
             self.databuffer[(i / 16) * 16 + i % 16] = data.data[i]
             #print(data.data[i])
-            self.asccibuffer += data.data[(i / 4) * 4 + i % 4]
+            self.asccibuffer += data.data[i]#(i % 4) * 16 + i / 4]
 
         """
         for i in range(64):
