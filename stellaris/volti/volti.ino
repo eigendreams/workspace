177cf5149e0c850f9a301a043a158d68b6759a8b
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2015-01-22 15:51:05 -0600

    float32 3 data

--- stellaris/volti/volti.ino
+++ stellaris/volti/volti.ino
@@ -1,6 +1,6 @@
 #include "ros.h"
 #include "std_msgs/Int16.h"
-#include "volti/float32_12.h"
+#include "volti/float32_3.h"
 #include "Servo.h"
 #include "AS5043.h"
 #include "Encoder.h"
@@ -13,11 +13,11 @@ unsigned long milisLast = 0;
 unsigned long milisLastMsg = 0;
 bool timedOut = false;
 
-volti::float32_12 imu_data1;
+volti::float32_3 imu_data1;
 ros::Publisher imu_pub1("i1", &imu_data1);
 IMU imu1(1);
 
-volti::float32_12 imu_data2;
+volti::float32_3 imu_data2;
 ros::Publisher imu_pub2("i2", &imu_data2);
 IMU imu2(3);
 
@@ -179,7 +179,7 @@ void loop() {
     imu_data1.data[0] = TO_DEG(imu1.yaw);
     imu_data1.data[1] = TO_DEG(imu1.pitch);
     imu_data1.data[2] = TO_DEG(imu1.roll);
-    imu_data1.data[3] = imu1.accel[0];
+    /*imu_data1.data[3] = imu1.accel[0];
     imu_data1.data[4] = imu1.accel[1];
     imu_data1.data[5] = imu1.accel[2];
     imu_data1.data[6] = imu1.magnetom[0];
@@ -187,14 +187,14 @@ void loop() {
     imu_data1.data[8] = imu1.magnetom[2];
     imu_data1.data[9] = imu1.gyro[0];
     imu_data1.data[10] = imu1.gyro[1];
-    imu_data1.data[11] = imu1.gyro[2];
+    imu_data1.data[11] = imu1.gyro[2];*/
 
     imu_pub1.publish(&imu_data1);
 
     imu_data2.data[0] = TO_DEG(imu2.yaw);
     imu_data2.data[1] = TO_DEG(imu2.pitch);
     imu_data2.data[2] = TO_DEG(imu2.roll);
-    imu_data2.data[3] = imu2.accel[0];
+    /*imu_data2.data[3] = imu2.accel[0];
     imu_data2.data[4] = imu2.accel[1];
     imu_data2.data[5] = imu2.accel[2];
     imu_data2.data[6] = imu2.magnetom[0];
@@ -202,7 +202,7 @@ void loop() {
     imu_data2.data[8] = imu2.magnetom[2];
     imu_data2.data[9] = imu2.gyro[0];
     imu_data2.data[10] = imu2.gyro[1];
-    imu_data2.data[11] = imu2.gyro[2];
+    imu_data2.data[11] = imu2.gyro[2];*/
 
     imu_pub2.publish(&imu_data2);
   }
