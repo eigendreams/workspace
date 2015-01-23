1522baeb971ea7efab7bbf79f34ca99c193f6aa9
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2015-01-23 01:34:16 -0600

    inc

--- catkin/src/volti/nodes/imu_node.cpp
+++ catkin/src/volti/nodes/imu_node.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Float32.h"
 
 #include <sstream>
 
@@ -7,7 +8,7 @@
 #include "imu_lib.h"
 #include <cmath>
 
-#include "volti/float32_3.h"
+//#include "volti/float32_3.h"
 
 using namespace std;
 
@@ -17,18 +18,21 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "imu_node");
   ros::NodeHandle n;
-  ros::Publisher imu_pub = n.advertise<volti::float32_3>("i1", 1000);
+  //ros::Publisher imu_pub = n.advertise<volti::float32_3>("i1", 1000);
+  ros::Publisher imu_pub = n.advertise<std_msgs::Float32>("i1", 1000);
   ros::Rate loop_rate(50);
   int count = 0;
   IMU MyImu;
-  volti::float32_3 msg;
+  //volti::float32_3 msg;
+  std_msgs::Float32 msg;
 
   while (ros::ok())
   {
     MyImu.loop();
-    msg.data[0] = MyImu.pitch*180.0/pi;
-    msg.data[0] = MyImu.roll*180.0/pi;
-    msg.data[0] = MyImu.yaw*180.0/pi;
+    //msg.data[0] = MyImu.pitch*180.0/pi;
+    //msg.data[0] = MyImu.roll*180.0/pi;
+    //msg.data[0] = MyImu.yaw*180.0/pi;
+    msg.data = MyImu.pitch*180.0/pi;
     imu_pub.publish(msg);
     ros::spinOnce();
     loop_rate.sleep();
