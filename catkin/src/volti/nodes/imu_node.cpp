54d937d81f223d6daa70dfc6d475513dc1d5e973
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2015-01-27 16:46:32 -0600

    imu node temp fix

--- catkin/src/volti/nodes/imu_node.cpp
+++ catkin/src/volti/nodes/imu_node.cpp
@@ -9,7 +9,7 @@
 #include <cmath>
 
 //#include "volti/float32_12.h"
-#include "volti/float32_3.h"
+//#include "volti/float32_3.h"
 
 using namespace std;
 
@@ -19,12 +19,12 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "imu_node");
   ros::NodeHandle n;
-  ros::Publisher imu_pub = n.advertise<volti::float32_3>("i1", 1000);
+  //ros::Publisher imu_pub = n.advertise<volti::float32_3>("i1", 1000);
   //ros::Publisher imu_pub = n.advertise<std_msgs::Float32>("i1", 1000);
   ros::Rate loop_rate(50);
   int count = 0;
   IMU MyImu;
-  volti::float32_3 msg;
+  //volti::float32_3 msg;
   //std_msgs::Float32 msg;
 
   while (ros::ok())
@@ -32,9 +32,9 @@ int main(int argc, char **argv)
     MyImu.loop();
 
     if (count % 5 == 0) {
-        msg.data[0] = MyImu.yaw * 180 / pi;
-        msg.data[1] = MyImu.pitch * 180 / pi;
-        msg.data[2] = MyImu.roll * 180 / pi;
+        //msg.data[0] = MyImu.yaw * 180 / pi;
+        //msg.data[1] = MyImu.pitch * 180 / pi;
+        //msg.data[2] = MyImu.roll * 180 / pi;
         /*msg.data[3] = MyImu.accel[0];
         msg.data[4] = MyImu.accel[1];
         msg.data[5] = MyImu.accel[2];
@@ -44,7 +44,7 @@ int main(int argc, char **argv)
         msg.data[9] = MyImu.gyro[0];
         msg.data[10] = MyImu.gyro[1];
         msg.data[11] = MyImu.gyro[2];*/
-        imu_pub.publish(msg);
+        //imu_pub.publish(msg);
     }
 
     //msg.data = MyImu.accel[0];
