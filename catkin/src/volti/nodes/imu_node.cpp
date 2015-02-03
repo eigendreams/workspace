bb56e4261a7ae2f2e0d48b98efcc0995b6ed1626
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2015-02-03 14:45:29 -0600

    imu test

--- catkin/src/volti/nodes/imu_node.cpp
+++ catkin/src/volti/nodes/imu_node.cpp
@@ -19,35 +19,47 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "imu_node");
   ros::NodeHandle n;
-
-  ros::Publisher imu_pub = n.advertise<volti::float32_12>("i1", 1000);
-  //ros::Publisher imu_pub = n.advertise<volti::float32_3>("i1", 1000);
-  //ros::Publisher imu_pub = n.advertise<std_msgs::Float32>("i1", 1000);
-
+  ros::Publisher imu1_pub = n.advertise<volti::float32_12>("i1", 1000);
+  ros::Publisher imu2_pub = n.advertise<volti::float32_12>("i2", 1000);
   ros::Rate loop_rate(50);
-  IMU MyImu(2);
-
-  volti::float32_12 msg;
-  //volti::float32_3 msg;
-  //std_msgs::Float32 msg;
+  IMU MyImu1(1);
+  IMU MyImu2(2);
+  volti::float32_12 msg1;
+  volti::float32_12 msg2;
 
   while (ros::ok())
   {
-    MyImu.loop();
+    MyImu1.loop();
 
-    msg.data[0] = MyImu.yaw * 180 / pi;
-    msg.data[1] = MyImu.pitch * 180 / pi;
-    msg.data[2] = MyImu.roll * 180 / pi;
-    msg.data[3] = MyImu.accel[0];
-    msg.data[4] = MyImu.accel[1];
-    msg.data[5] = MyImu.accel[2];
-    msg.data[6] = MyImu.magnetom[0];
-    msg.data[7] = MyImu.magnetom[1];
-    msg.data[8] = MyImu.magnetom[2];
-    msg.data[9] = MyImu.gyro[0];
-    msg.data[10] = MyImu.gyro[1];
-    msg.data[11] = MyImu.gyro[2];
-    imu_pub.publish(msg);
+    msg1.data[0] = MyImu1.yaw * 180 / pi;
+    msg1.data[1] = MyImu1.pitch * 180 / pi;
+    msg1.data[2] = MyImu1.roll * 180 / pi;
+    msg1.data[3] = MyImu1.accel[0];
+    msg1.data[4] = MyImu1.accel[1];
+    msg1.data[5] = MyImu1.accel[2];
+    msg1.data[6] = MyImu1.magnetom[0];
+    msg1.data[7] = MyImu1.magnetom[1];
+    msg1.data[8] = MyImu1.magnetom[2];
+    msg1.data[9] = MyImu1.gyro[0];
+    msg1.data[10] = MyImu1.gyro[1];
+    msg1.data[11] = MyImu1.gyro[2];
+    imu1_pub.publish(msg1);
+
+    MyImu2.loop();
+
+    msg2.data[0] = MyImu2.yaw * 180 / pi;
+    msg2.data[1] = MyImu2.pitch * 180 / pi;
+    msg2.data[2] = MyImu2.roll * 180 / pi;
+    msg2.data[3] = MyImu2.accel[0];
+    msg2.data[4] = MyImu2.accel[1];
+    msg2.data[5] = MyImu2.accel[2];
+    msg2.data[6] = MyImu2.magnetom[0];
+    msg2.data[7] = MyImu2.magnetom[1];
+    msg2.data[8] = MyImu2.magnetom[2];
+    msg2.data[9] = MyImu2.gyro[0];
+    msg2.data[10] = MyImu2.gyro[1];
+    msg2.data[11] = MyImu2.gyro[2];
+    imu2_pub.publish(msg2);
     
     ros::spinOnce();
     loop_rate.sleep();
