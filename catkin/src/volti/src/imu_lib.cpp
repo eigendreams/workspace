8538a4ef4051337371f2f337710404532e044750
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2015-02-09 13:51:16 -0600

    incremental

--- catkin/src/volti/src/imu_lib.cpp
+++ catkin/src/volti/src/imu_lib.cpp
@@ -133,9 +133,10 @@ float IMU::constrain(float X, float A, float B) {
 
 void IMU::Matrix_update(void)
 {
-  Gyro_Vector[0]=GYRO_SCALED_RAD(gyro[0]); //gyro x roll
-  Gyro_Vector[1]=GYRO_SCALED_RAD(gyro[1]); //gyro y pitch
-  Gyro_Vector[2]=GYRO_SCALED_RAD(gyro[2]); //gyro z yaw
+  Gyro_Vector[0]=1.1 * GYRO_SCALED_RAD(gyro[0]); //gyro x roll
+  Gyro_Vector[1]=1.1 * GYRO_SCALED_RAD(gyro[1]); //gyro y pitch
+  Gyro_Vector[2]=1.1 * ars
+  GYRO_SCALED_RAD(gyro[2]); //gyro z yaw
   
   Accel_Vector[0]=accel[0];
   Accel_Vector[1]=accel[1];
@@ -626,7 +627,7 @@ void IMU::loop()
     
     // Update sensor readings
     //Wire.setModule(module);
-    G_Dt = .02;
+    G_Dt = .020;
     read_sensors();
 
     // Apply sensor calibration
