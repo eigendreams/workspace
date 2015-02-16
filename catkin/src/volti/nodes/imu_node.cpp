fed05c5a34d5e5d0e0feb5597af676a8a0c6477f
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2015-02-16 15:49:15 -0600

    incremental

--- catkin/src/volti/nodes/imu_node.cpp
+++ catkin/src/volti/nodes/imu_node.cpp
@@ -14,6 +14,187 @@ using namespace std;
 
 float pi= 3.1415926535897;
 
+///////////////////////////////////////////////////////////////////////////////
+
+class Kalman1D {
+
+public:
+
+  Kalman1D() {
+    times = 0;
+    dt = 0.020;
+  }
+
+  void transpose(float destination[2][2], float A[2][2]) {
+    destination[0][0] = A[0][0];
+    destination[0][1] = A[1][0];
+    destination[1][0] = A[0][1];
+    destination[1][1] = A[1][1];
+  }
+
+  void inverse(float destination[2][2], float A[2][2]) {
+    float a = A[0][0];
+    float c = A[1][0];
+    float b = A[0][1];
+    float d = A[1][1];
+
+    float divisor = (a*d - b * c);
+
+    destination[0][0] = d / divisor;
+    destination[1][0] = -c / divisor;
+    destination[0][1] = -b / divisor;
+    destination[1][1] = a / divisor;
+  }
+
+  void minusVectorVector(float destination[2][1], float A[2][1], float B[2][1]) {
+    destination[0][0] = A[0][0] - B[0][0];
+    destination[1][0] = A[1][0] - B[1][0];
+  }
+
+  void sumVectorVector(float destination[2][1], float A[2][1], float B[2][1]) {
+    destination[0][0] = A[0][0] + B[0][0];
+    destination[1][0] = A[1][0] + B[1][0];
+  }
+
+  void minusMatrixMatrix(float destination[2][2], float A[2][2], float B[2][2]) {
+    destination[0][0] = A[0][0] - B[0][0];
+    destination[0][1] = A[0][1] - B[0][1];
+    destination[1][0] = A[1][0] - B[1][0];
+    destination[1][1] = A[1][1] - B[1][1];
+  }
+
+  void sumMatrixMatrix(float destination[2][2], float A[2][2], float B[2][2]) {
+    destination[0][0] = A[0][0] + B[0][0];
+    destination[0][1] = A[0][1] + B[0][1];
+    destination[1][0] = A[1][0] + B[1][0];
+    destination[1][1] = A[1][1] + B[1][1];
+  }
+
+  void copyVector(float destination[2][1], float origin[2][1]) {
+    destination[0][0] = origin[0][0];
+    destination[1][0] = origin[1][0];
+  }
+
+  void copyMatrix(float destination[2][2], float origin[2][2]) {
+    destination[0][0] = origin[0][0];
+    destination[0][1] = origin[0][1];
+    destination[1][0] = origin[1][0];
+    destination[1][1] = origin[1][1];
+  }
+
+  void multMatrixMatrix(float destination[2][2], float A[2][2], float B[2][2] ) {
+    destination[0][0] = A[0][0] * B[0][0] + A[0][1] * B[1][0];
+    destination[0][1] = A[0][0] * B[0][1] + A[0][1] * B[1][1];
+    destination[1][0] = A[1][0] * B[0][0] + A[1][1] * B[1][0];
+    destination[1][1] = A[1][0] * B[0][1] + A[1][1] * B[1][1];
+  }
+
+  void multMatrixVector(float destination[2][1], float A[2][2], float V[2][1]) {
+    destination[0][0] = A[0][0] * V[0][0] + A[0][1] * V[1][0];
+    destination[1][0] = A[1][0] * V[0][0] + A[1][1] * V[1][0];
+  }
+
+  float multVectorVector(float V1[2][1], float V2[2][1]) {
+    return ((V1[0][0]) * (V2[0][0]) + (V1[1][0]) * (V2[1][0]));
+  }
+
+  float A [2][2]      = {{1, dt}, {0, 1}};
+  float H [2][2]      = {{1, 0}, {0, 1}};
+  float Y [2][1]      = {{0}, {0}};
+  float Ym1 [2][1]    = {{0}, {0}};
+  float Ym1m1 [2][1]  = {{0}, {0}};
+  float Xkp [2][1]    = {{0}, {0}};
+  float Xk[2][1]      = {{0}, {0}};
+  float Xkm1 [2][1]   = {{0}, {0}};
+  float Xkm1m1 [2][1] = {{0}, {0}};
+  float Q[2][2]       = {{10, 0}, {0, 10}};
+  float R[2][2]       = {{10, 0}, {0, 10}};
+  float Pkp [2][2]    = {{10, 0}, {0, 10}};
+  float Pk[2][2]      = {{0, 0}, {0, 0}};
+  float Pkm1 [2][2]   = {{0, 0}, {0, 0}};
+  float Vk [2][1]     = {{0}, {0}};
+  float Sk [2][2]     = {{0, 0}, {0, 0}};
+  float Kk [2][2]     = {{0, 0}, {0, 0}};
+
+  float TM1 [2][2]    = {{0, 0}, {0, 0}};
+  float TM2 [2][2]    = {{0, 0}, {0, 0}};
+  float TM3 [2][2]    = {{0, 0}, {0, 0}};
+  float TV1 [2][1]    = {{0}, {0}};
+  float TV2 [2][1]    = {{0}, {0}};
+  float TV3 [2][1]    = {{0}, {0}};
+
+  long times;
+  float dt;
+
+  void compute(float measure) {
+
+    times++;
+
+    if (times < 4) {
+
+      float startV [2][1] = {{measure}, {0}};
+
+      copyVector(Y, startV);
+      copyVector(Ym1, startV);
+      copyVector(Ym1m1, startV);
+      copyVector(Xk, startV);
+      copyVector(Xkm1, startV);
+      copyVector(Xkm1m1, startV);
+    }
+
+    copyVector(Ym1m1, Ym1);
+    copyVector(Ym1, Y);
+    copyVector(Xkm1m1, Xkm1);
+    copyVector(Xkm1, Xk);
+
+
+    multMatrixVector(Xkp, A, Xkm1);
+
+
+    transpose(TM1, A);
+    multMatrixMatrix(TM2, Pkm1, TM1);
+    multMatrixMatrix(TM3, A, TM2);
+    sumMatrixMatrix(Pkp, TM3, Q);
+
+
+    Y[0][0] = measure;
+    Y[1][0] = (measure - Ym1[1][0]) / dt;
+    Y[2][0] = (Ym1[1][0] - Ym1m1[1][0]) / dt;
+
+
+    multMatrixVector(TV1, H, Xkp);
+    minusVectorVector(Vk, Y, TV1);
+
+
+    transpose(TM1, H);
+    multMatrixMatrix(TM2, Pkp, TM1);
+    multMatrixMatrix(TM3, H, TM2);
+    sumMatrixMatrix(Sk, TM3, R);
+
+
+    transpose(TM1, H);
+    inverse(TM2, Sk);
+    multMatrixMatrix(TM3, TM1, TM2);
+    multMatrixMatrix(Kk, Pkp, TM3);
+
+    multMatrixVector(TV1, Kk, Vk);
+    sumVectorVector(Xk, Xkp, TV1);
+
+    transpose(TM1, Kk);
+    multMatrixMatrix(TM2, Sk, TM1);
+    multMatrixMatrix(TM3, Kk, TM2);
+    minusMatrixMatrix(Pk, Pkp, TM3);
+
+    copyMatrix(Pkm1, Pk);
+  }
+
+  float get() {
+    return Xk[0][0];
+  }
+
+};
+///////////////////////////////////////////////////////////////////////////////
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "imu_node");
@@ -32,13 +213,27 @@ int main(int argc, char **argv)
   volti::float32_3 imu_plate_msg;
   volti::float32_3 imu_pendu_msg;
 
+  Kalman1D kalman1;
+  Kalman1D kalman2;
+  Kalman1D kalman3;
+  Kalman1D kalman4;
+  Kalman1D kalman5;
+  Kalman1D kalman6;
+  Kalman1D kalman7;
+  Kalman1D kalman8;
+  Kalman1D kalman9;
+
   while (ros::ok())
   {
     imu_plate.loop();
 
-    imu_plate_msg.data[0] = imu_plate.roll;
-    imu_plate_msg.data[1] = imu_plate.pitch;
-    imu_plate_msg.data[2] = imu_plate.yaw;
+    kalman1.compute(imu_plate.roll);
+    kalman2.compute(imu_plate.pitch);
+    kalman3.compute(imu_plate.yaw);
+
+    imu_plate_msg.data[0] = kalman1.get();
+    imu_plate_msg.data[1] = kalman2.get();
+    imu_plate_msg.data[2] = kalman3.get();
     /*imu_plate_msg.data[3] = imu_plate.accel[0] * 9.806 / 256.0;
     imu_plate_msg.data[4] = imu_plate.accel[1] * 9.806 / 256.0;
     imu_plate_msg.data[5] = imu_plate.accel[2] * 9.806 / 256.0;
@@ -52,9 +247,13 @@ int main(int argc, char **argv)
 
     imu_pendu.loop();
 
-    imu_pendu_msg.data[0] = imu_pendu.roll;
-    imu_pendu_msg.data[1] = imu_pendu.pitch;
-    imu_pendu_msg.data[2] = imu_pendu.yaw;
+    kalman4.compute(imu_pendu.roll);
+    kalman5.compute(imu_pendu.pitch);
+    kalman6.compute(imu_pendu.yaw);
+
+    imu_pendu_msg.data[0] = kalman4.get();
+    imu_pendu_msg.data[1] = kalman5.get();
+    imu_pendu_msg.data[2] = kalman6.get();
     /*imu_pendu_msg.data[3] = imu_pendu.accel[0] * 9.806 / 256.0;
     imu_pendu_msg.data[4] = imu_pendu.accel[1] * 9.806 / 256.0;
     imu_pendu_msg.data[5] = imu_pendu.accel[2] * 9.806 / 256.0;
--- catkin/src/volti/scripts/top_control.py
+++ catkin/src/volti/scripts/top_control.py
@@ -21,37 +21,11 @@ def processIMU1_message(rawMsg):
 
     roll = rawMsg.data[0]
     pitch = rawMsg.data[1]
-    yaw = rawMsg.data[2]
-
-    R_yaw_z = array([[cos(yaw), sin(yaw), 0], [-sin(yaw), cos(yaw), 0], [0, 0, 1]])
-    R_pitch_y = array([[cos(pitch), 0, sin(pitch)],[0, 1, 0],[-sin(pitch), 0, cos(pitch)]])
-    R_roll_x = array([[1, 0, 0],[0, cos(roll), -sin(roll)],[0, sin(roll), cos(roll)]])
-   
-    x_axis_imu = (dot(R_yaw_z, dot(R_pitch_y, dot(R_roll_x, array([[1],[0],[0]])))))
-    y_axis_imu = (dot(R_yaw_z, dot(R_pitch_y, dot(R_roll_x, array([[0],[1],[0]])))))
-    z_axis_imu = (dot(R_yaw_z, dot(R_pitch_y, dot(R_roll_x, array([[0],[0],[1]])))))
-    
-    x_test.axis = x_axis_imu
-    y_test.axis = y_axis_imu
-    z_test.axis = z_axis_imu
 
 def processIMU2_message(rawMsg):
 
     roll2 = rawMsg.data[0]
     pitch2 = rawMsg.data[1]
-    yaw2 = rawMsg.data[2]
-
-    R_yaw_z2 = array([[cos(yaw2), sin(yaw2), 0], [-sin(yaw2), cos(yaw2), 0], [0, 0, 1]])
-    R_pitch_y2 = array([[cos(pitch2), 0, sin(pitch2)],[0, 1, 0],[-sin(pitch2), 0, cos(pitch2)]])
-    R_roll_x2 = array([[1, 0, 0],[0, cos(roll2), -sin(roll2)],[0, sin(roll2), cos(roll2)]])
-
-    x_axis_imu2 = dot(R_yaw_z2, dot(R_pitch_y2, dot(R_roll_x2, array([[1],[0],[0]]))))
-    y_axis_imu2 = dot(R_yaw_z2, dot(R_pitch_y2, dot(R_roll_x2, array([[0],[1],[0]]))))
-    z_axis_imu2 = dot(R_yaw_z2, dot(R_pitch_y2, dot(R_roll_x2, array([[0],[0],[1]]))))
-
-    x_test_2.axis = x_axis_imu2
-    y_test_2.axis = y_axis_imu2
-    z_test_2.axis = z_axis_imu2
 
 sub1 = rospy.Subscriber('imu_plate_3', float32_3, processIMU1_message)
 sub2 = rospy.Subscriber('imu_pendu_3', float32_3, processIMU2_message)
