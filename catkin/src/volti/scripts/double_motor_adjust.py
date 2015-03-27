11235ac20c25436494144dfefd927e4bb7b54030
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2015-03-27 17:41:24 -0600

    inc

--- catkin/src/volti/scripts/double_motor_adjust.py
+++ catkin/src/volti/scripts/double_motor_adjust.py
@@ -398,7 +398,7 @@ class Double_motor:
         if (abs(self.actual_error) < self.minimal_error):
             self.minimal_error = constrain(abs(self.actual_error), 0, 0.4)
         #
-        self.tmp_constrain = 20 - 10 * (1 - map(self.minimal_error, 0, 0.4, 0, 1))
+        self.tmp_constrain = 20 - 7 * (1 - map(self.minimal_error, 0, 0.4, 0, 1))
         #
         #
         #rospy.loginfo("salida: " + str(self.salida_control_angulo) + " minerror: " + str(self.minimal_error))
