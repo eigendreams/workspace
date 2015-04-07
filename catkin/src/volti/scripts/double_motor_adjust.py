fd3c26e307e36c3a1052519ca70b54f4c70688e6
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2015-04-07 11:15:18 -0500

    inc

--- catkin/src/volti/scripts/double_motor_adjust.py
+++ catkin/src/volti/scripts/double_motor_adjust.py
@@ -287,8 +287,8 @@ class Double_motor:
         self.X_plate   = self.filter_plate.compute(self.ang_plate)
         self.X_pendu   = self.filter_pendu.compute(self.ang_pendu)
         #
-        self.ang_plate = self.X_plate[0, 0]
-        self.ang_pendu = self.X_pendu[0, 0]
+        #self.ang_plate = self.X_plate[0, 0]
+        #self.ang_pendu = self.X_pendu[0, 0]
         #
         self.vel_plate = self.X_plate[1, 0]
         self.vel_pendu = self.X_pendu[1, 0]
