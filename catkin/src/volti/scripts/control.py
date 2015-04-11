9029e7799e2d14ad3c5a9793e0983d7598327da3
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2015-04-10 20:01:39 -0500

    inc

--- catkin/src/volti/scripts/control.py
+++ catkin/src/volti/scripts/control.py
@@ -388,13 +388,11 @@ class Control:
         self.pidvelm1msg.kd  = self.vel_settings['kd'] * self.accel_m1 
         self.pidvelm1msg.km  = self.vel_settings['km'] * self.vel_m1_des
         self.pidvelm1msg.sub = (self.vel_settings['kp'] + self.vel_settings['km']) * self.ang_control / self.pos_settings['div_ang2vel']
-        self.pidvelm1msg.out = self.salida_m1_vel 
         #
         self.pidvelm2msg.kp  = self.vel_settings['kp'] * self.vel_m2_err
         self.pidvelm2msg.kd  = self.vel_settings['kd'] * self.accel_m2 
         self.pidvelm2msg.km  = self.vel_settings['km'] * self.vel_m2_des
         self.pidvelm2msg.sub = (self.vel_settings['kp'] + self.vel_settings['km']) * -self.ang_control / self.pos_settings['div_ang2vel']
-        self.pidvelm2msg.out = self.salida_m2_vel 
         #
         #
         #
@@ -411,6 +409,8 @@ class Control:
         self.out_pos_m2 = self.profile_m2.compute( -self.salida_m1_ang * self.decexp + self.salida_m2_vel )
         #
         #
+        self.pidvelm1msg.out = self.salida_m1_vel 
+        self.pidvelm2msg.out = self.salida_m2_vel 
         #
         #
         self.pidvelm1vel.publish(self.pidvelm1msg)
