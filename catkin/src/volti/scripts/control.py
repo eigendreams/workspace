033f8c85b90afb995c97c362cf8650396cfe780a
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2015-04-10 20:00:02 -0500

    inc

--- catkin/src/volti/scripts/control.py
+++ catkin/src/volti/scripts/control.py
@@ -382,17 +382,6 @@ class Control:
         self.vel_m1_err = self.vel_m1_des - self.speed_m1
         self.vel_m2_err = self.vel_m2_des - self.speed_m2
         #
-        self.salida_m1_vel = self.pidvelm1msg.kp - self.pidvelm1msg.kd + self.pidvelm1msg.km
-        self.salida_m2_vel = self.pidvelm2msg.kp - self.pidvelm2msg.kd + self.pidvelm2msg.km
-        #
-        self.salida_m1_vel = self.salida_m1_vel + (self.vel_settings['kp'] + self.vel_settings['km']) * self.ang_control / self.pos_settings['div_ang2vel']
-        self.salida_m2_vel = self.salida_m2_vel + (self.vel_settings['kp'] + self.vel_settings['km']) * -self.ang_control / self.pos_settings['div_ang2vel']
-        #
-        self.salida_m1_vel = constrain(self.salida_m1_vel, -self.vel_settings['range'], self.vel_settings['range'])
-        self.salida_m2_vel = constrain(self.salida_m2_vel, -self.vel_settings['range'], self.vel_settings['range'])
-        #
-        self.out_pos_m1 = self.profile_m1.compute( self.salida_m1_ang * self.decexp + self.salida_m1_vel )
-        self.out_pos_m2 = self.profile_m2.compute( -self.salida_m1_ang * self.decexp + self.salida_m2_vel )
         #
         #
         self.pidvelm1msg.kp  = self.vel_settings['kp'] * self.vel_m1_err
@@ -406,6 +395,22 @@ class Control:
         self.pidvelm2msg.km  = self.vel_settings['km'] * self.vel_m2_des
         self.pidvelm2msg.sub = (self.vel_settings['kp'] + self.vel_settings['km']) * -self.ang_control / self.pos_settings['div_ang2vel']
         self.pidvelm2msg.out = self.salida_m2_vel 
+        #
+        #
+        #
+        self.salida_m1_vel = self.pidvelm1msg.kp - self.pidvelm1msg.kd + self.pidvelm1msg.km
+        self.salida_m2_vel = self.pidvelm2msg.kp - self.pidvelm2msg.kd + self.pidvelm2msg.km
+        #
+        self.salida_m1_vel = self.salida_m1_vel + (self.vel_settings['kp'] + self.vel_settings['km']) * self.ang_control / self.pos_settings['div_ang2vel']
+        self.salida_m2_vel = self.salida_m2_vel + (self.vel_settings['kp'] + self.vel_settings['km']) * -self.ang_control / self.pos_settings['div_ang2vel']
+        #
+        self.salida_m1_vel = constrain(self.salida_m1_vel, -self.vel_settings['range'], self.vel_settings['range'])
+        self.salida_m2_vel = constrain(self.salida_m2_vel, -self.vel_settings['range'], self.vel_settings['range'])
+        #
+        self.out_pos_m1 = self.profile_m1.compute( self.salida_m1_ang * self.decexp + self.salida_m1_vel )
+        self.out_pos_m2 = self.profile_m2.compute( -self.salida_m1_ang * self.decexp + self.salida_m2_vel )
+        #
+        #
         #
         #
         self.pidvelm1vel.publish(self.pidvelm1msg)
